dd8ab3f811f6731c97d8b7a7a66d74f3fab8382b
sixstringmark <mark@mckenzieduo.com>
2024-04-07 22:41:43 -0500

    fixed back buttton, add content

--- script.js
+++ script.js
@@ -1,63 +1,95 @@
-window.onload = function()
-{
+window.onload = function () {
     const path = window.location.pathname.split("/");
-
-    switch(path[1])
-    {
-        case "":
-        {
-            loadPage("home");
-            break;
-        }
-        case "about":
-        {
-            loadPage("about");
-            break;
-        }
-        case "pricing":
-        {
-            loadPage("pricing");
-            break;
-        }
-        default:
-        {
-            loadPage("404");
-            break;
-        }
-    }
-
-    document.querySelectorAll(".menu__item").forEach((item) =>
-    {
-        item.addEventListener("click", function()
-        {
-            const path = item.getAttribute("value");
-            loadPage(path);
-            if(path == "home")
+    console.log('bully')
+    switch (path[1]) {
+        case "": case "home":
             {
-                window.history.pushState("", "", "/");
-                return;
+                loadPage("home");
+                break;
+            }
+        case "about":
+            {
+                loadPage("about");
+                break;
+            }
+        case "listen":
+            {
+                loadPage("listen");
+                break;
+            }
+        case "contact":
+            {
+                loadPage("contact");
+                break;
+            }
+        case "endorsements":
+            {
+                loadPage("endorsements");
+                break;
+            }
+        case "songlist":
+            {
+                loadPage("songlist");
+                break;
+            }
+        case "contact":
+            {
+                loadPage("contact");
+                break;
             }
 
-            window.history.pushState("", "", path);
+        default:
+            {
+                loadPage("404");
+                break;
+            }
+    }
+
+    document.querySelectorAll(".menu__item").forEach((item) => {
+        item.addEventListener("click", function () {
+            const path = item.getAttribute("value");
+            console.log("path",path);
+            loadPage(path);
+            if (path == "home") {
+                window.history.pushState({ick:""}, "", "/");
+                return;
+            }
+            console.log("pushing " + path);
+            window.history.pushState({ick:path}, "", path);
         });
     });
 
-    function loadPage($path)
-    {
-        if($path == "") return;
+    function loadPage($path) {
+        if ($path == "") return;
 
         const container = document.getElementById("container");
-
+        console.log("get " + $path);
         const request = new XMLHttpRequest();
         request.open("GET", "pages/" + $path + ".html");
         request.send();
-        request.onload = function()
-        {
-            if(request.status == 200)
-            {
+        request.onload = function () {
+            if (request.status == 200) {
                 container.innerHTML = request.responseText;
                 document.title = $path;
             }
         }
     }
+
+    // Handle forward/back buttons
+window.addEventListener("popstate", (event) => {
+    // If a state has been provided, we have a "simulated" page
+    // and we update the current page.
+    if (event) {
+        const state = event.state;
+        if(state.ick) {
+            loadPage(state.ick);
+        }
+      // Simulate the loading of the previous page
+      console.log("event.state", event);
+    }
+  });
+
 }
+
+
+  history.replaceState({ick:"home"}, "");
