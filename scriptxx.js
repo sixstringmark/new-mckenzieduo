9f34aee05051987274e99d351f9321f04297be22
sixstringmark <mark@mckenzieduo.com>
2024-04-23 00:22:02 -0500

    duo sketch png images etc

--- scriptxx.js
+++ scriptxx.js
@@ -1,27 +1,46 @@
 const player_config = {};
 
+/**
+ * We use a global config for the audio player. There is only a single
+ * <audio> object, but it could be played from multiple play links. A page
+ * may have optionally have a master play button, now-playing label, and progress bar.
+ * 
+ * This is to offer the convenience of displaying a simple play icon on any page,
+ * with no controls other than play/pause and no other change in state or progress.
+ * 
+ * Sorry, only one master and progress bar per page is supported. 
+ * 
+ * 
+ */
 function initPlayer() {
     console.log('initPlayer');
+    
     player_config.audio = document.getElementById("audio");
+    player_config.audio.pause();
+    player_config.audio.addEventListener("timeupdate", trackTimeChanged, false);
+    player_config.audio.addEventListener("ended", function (e) {
+        trackHasEnded();
+    }, false);
+
     player_config.play_master = document.getElementById("play_master");
     player_config.play_label = document.getElementById("play_label");
     player_config.progress = document.getElementById("pp");
     player_config.buffer = document.getElementById("pp2");
     player_config.now_playing = null;
     player_config.progress_label = document.getElementById("pp_label");
-    player_config.progress.onclick = function (event) { player_seek(event); };
-    player_config.audio.addEventListener("timeupdate", trackTimeChanged, false);
-    player_config.audio.addEventListener("ended", function (e) {
-        trackHasEnded();
-    }, false);
+    if( player_config.progress ) {
+        player_config.progress.onclick = function (event) { player_seek(event); };
+    }
 }
 
 function bufferProgress() {
-    if (player_config.audio.buffered.end.length > 0) {
-        var bufferedTime = (player_config.audio.buffered.end(0) * 100) / player_config.audio.duration;
-        player_config.buffer.value = bufferedTime;
-    } else {
-        player_config.buffer.value = 100;
+    if (player_config.progress) {
+        if (player_config.audio.buffered.end.length > 0) {
+            var bufferedTime = (player_config.audio.buffered.end(0) * 100) / player_config.audio.duration;
+            player_config.buffer.value = bufferedTime;
+        } else {
+            player_config.buffer.value = 100;
+        }
     }
 };
 
@@ -46,8 +65,10 @@ function playMe(songURL) {
     console.log(songURL);
     player_config.audio.setAttribute("src", songURL);
     player_config.audio.load();
-    player_config.progress.value = 0;
-    player_config.buffer.value = 0;
+    if( player_config.progress ) {
+        player_config.progress.value = 0;
+        player_config.buffer.value = 0;
+    }
     player_config.audio.play();
 
 
@@ -63,11 +84,15 @@ function togglePlay(song) {
         if (player_config.now_playing === song) {
             // if playing, pause it, otherwise start it
             if (player_config.audio.paused) {
-                player_config.play_master.classList.add('gv_icon_pause');
+                if( player_config.play_master) {
+                    player_config.play_master.classList.add('gv_icon_pause');
+                }
                 song.classList.add('gv_icon_pause');
                 player_config.audio.play();
             } else {
-                player_config.play_master.classList.remove('gv_icon_pause');
+                if( player_config.play_master) {
+                    player_config.play_master.classList.remove('gv_icon_pause');
+                }
                 song.classList.remove('gv_icon_pause');
                 player_config.audio.pause();
             }
@@ -81,13 +106,19 @@ function togglePlay(song) {
             song.classList.add("gv_icon_pause");
             player_config.audio.setAttribute("src", song.getAttribute("data-song"));
             player_config.audio.load();
-            player_config.progress.value = 0;
-            player_config.buffer.value = 0;
+            if( player_config.progress ) {
+                player_config.progress.value = 0;
+                player_config.buffer.value = 0;
+            }
             player_config.audio.play();
-            player_config.play_master.classList.add('gv_icon_pause');
+            if( player_config.play_master) {
+                player_config.play_master.classList.add('gv_icon_pause');
+            }
             console.log('grrr');
             const lbl = song.parentNode.parentNode.getElementsByClassName("demoTitle");
-            player_config.play_label.innerHTML = lbl[0].textContent;
+            if( player_config.play_label ) {
+                player_config.play_label.innerHTML = lbl[0].textContent;
+            }
             const to_move = document.getElementById("progress_top");
             console.log(to_move);
 
@@ -100,19 +131,25 @@ function togglePlay(song) {
         }
     } else {
         // no song played yet - crank it up
+        player_config.audio.setAttribute("src", song.getAttribute("data-song"));
+        player_config.audio.load();
+        player_config.audio.play();
         song.parentNode.parentNode.classList.add("playlist_playing");
         player_config.now_playing = song;
         song.classList.add("gv_icon_pause");
-        player_config.audio.setAttribute("src", song.getAttribute("data-song"));
-        player_config.audio.load();
-        player_config.progress.value = 0;
-        player_config.buffer.value = 0;
-        player_config.audio.play();
-        player_config.play_master.classList.add('gv_icon_pause');
+        if( player_config.progress ) {
+            player_config.progress.value = 0;
+            player_config.buffer.value = 0;
+        }
+        if( player_config.play_master ) {
+            player_config.play_master.classList.add('gv_icon_pause');
+        }
         console.log('brrr');
         console.log(song.parentNode.parentNode);
         const lbl = song.parentNode.parentNode.getElementsByClassName("demoTitle");
-        player_config.play_label.innerHTML = lbl[0].textContent;
+        if( lbl && player_config.play_label) {
+            player_config.play_label.innerHTML = lbl[0].textContent;
+        }
         const to_move = document.getElementById("progress_top");
         console.log(to_move);
         //const move_to = song.parentNode.parentNode.getElementsByClassName("demoTitle")[0].appendChild(to_move);
@@ -133,12 +170,16 @@ function togglePlaying(control) {
     if (player_config.now_playing) {
         // if playing, pause it, otherwise start it
         if (player_config.audio.paused) {
-            player_config.play_master.classList.add('gv_icon_pause');
+            if( player_config.play_master) {
+                player_config.play_master.classList.add('gv_icon_pause');
+            }
             icon.classList.add('gv_icon_pause');
             player_config.now_playing.classList.add('gv_icon_pause');
             player_config.audio.play();
         } else {
-            player_config.play_master.classList.remove('gv_icon_pause');
+            if( player_config.play_master) {
+                player_config.play_master.classList.remove('gv_icon_pause');
+            }
             icon.classList.remove('gv_icon_pause');
             player_config.now_playing.classList.remove('gv_icon_pause');
             player_config.audio.pause();
@@ -150,27 +191,33 @@ function togglePlaying(control) {
 
 
 function trackTimeChanged() {
-    var currentTime = audio.currentTime;
-    var duration = audio.duration;
-    var p_duration = secondsToHHMMSS(duration);
-    var p_currentTime = secondsToHHMMSS(currentTime);
-    player_config.progress_label.innerHTML = p_currentTime + " / " + p_duration;
-    //console.log(player_config.progress_label.innerHTML);
-    //document.getElementById("progress_label").innerHTML = "whatever";
+    if (player_config.progress) {
+        var currentTime = audio.currentTime;
+        var duration = audio.duration;
+        var p_duration = secondsToHHMMSS(duration);
+        var p_currentTime = secondsToHHMMSS(currentTime);
+        player_config.progress_label.innerHTML = p_currentTime + " / " + p_duration;
+        //console.log(player_config.progress_label.innerHTML);
+        //document.getElementById("progress_label").innerHTML = "whatever";
 
-    if (duration && duration > 0) {
-        var pct = 100 * currentTime / duration;
-        player_config.progress.value = pct;
+        if (duration && duration > 0) {
+            var pct = 100 * currentTime / duration;
+            player_config.progress.value = pct;
+        }
+        bufferProgress();
     }
-    bufferProgress();
 }
 function trackHasEnded(e) {
     if (player_config.now_playing) {
         // if playing, pause it, otherwise start it
-        player_config.play_master.classList.remove('gv_icon_pause');
+        if( player_config.play_master) {
+            player_config.play_master.classList.remove('gv_icon_pause');
+        }
         player_config.now_playing.classList.remove('gv_icon_pause');
     }
-    player_config.progress.value = 0;
+    if( player_config.progress ) {
+        player_config.progress.value = 0;
+    }
 }
 function setProgress(p) {
     console.log("progress");
@@ -261,9 +308,10 @@ window.onload = function () {
         container.innerHTML = y;
         aside.innerHTML = yy;
 
-        if (xpath == "listen") {
-            initPlayer();
-        }
+        initPlayer();
+        // if (xpath == "listen") {
+        //     initPlayer();
+        // }
     }
 
     // Handle forward/back buttons
